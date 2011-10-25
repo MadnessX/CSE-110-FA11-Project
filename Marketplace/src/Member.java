f37750a94325291d23582bf1cb766b80c1bd5679
Tansen Zhu <tansen.zhu@gmail.com>
2011-10-25 09:54:17 -0700

    Basic gui: - login works with db file - viewing items works with db file

--- Marketplace/src/Member.java
+++ Marketplace/src/Member.java
@@ -14,12 +14,37 @@ public class Member {
 		this.name = name;
 		this. password = password;
 	}
-	
-	public int getMemberID(){
-		return this.memberID;
+
+	public int getMemberID() {
+		return memberID;
+	}
+
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
-	public String getName(){
-		return this.name;
-	}
 }
